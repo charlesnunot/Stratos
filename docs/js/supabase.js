9309b460950dab335cba741b8cd46523025f5ada
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-04 13:55:35 +0800

    Delete docs/js/supabase.js

--- docs/js/supabase.js
+++
@@ -1,8 +0,0 @@
-// js/supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase.min.js';
-
-const SUPABASE_URL = 'https://zquslphbmowkgrdlygza.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_oaojowgzWjzLUAUhA7rjfw_hntjdrcu';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
