2975854ddb115e3ba914f1dddf0c8a92cc3db394
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-04 17:44:36 +0800

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,11 +1,15 @@
-// main.js
-
 // 获取按钮和菜单元素
 const moreButton = document.getElementById('more-button');
 const sidebar = document.getElementById('sidebar');
 
 // 监听 More 按钮的点击事件
 moreButton.addEventListener('click', function() {
+  // 打印调试信息，检查点击事件是否触发
+  console.log('More button clicked');
+
   // 切换 "show-more" 类
   sidebar.classList.toggle('show-more');
+
+  // 打印当前是否添加了 "show-more" 类
+  console.log('Sidebar classes:', sidebar.classList);
 });
