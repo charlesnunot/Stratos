144907753ffc7623ca84ae1994e2cb02a9c3bd19
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-04 17:28:35 +0800

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,6 @@
+// 显示和隐藏侧边栏的更多菜单
+document.getElementById('more-button').addEventListener('click', () => {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('show-more');
+});
 
