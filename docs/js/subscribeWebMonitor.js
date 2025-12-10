e2ae8a13034d5177a7f6f9243e1be85d7a5aa879
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-10 13:40:21 +0800

    Update subscribeWebMonitor.js

--- docs/js/subscribeWebMonitor.js
+++ docs/js/subscribeWebMonitor.js
@@ -1,1 +1,22 @@
+import { supabase } from './userService.js';
+
+export function subscribeWebMonitor(uid, callback) {
+  const channel = supabase
+    .channel(`web_monitor-${uid}`, { config: { broadcast: { self: true } } })
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'public',
+        table: 'web_monitor',
+        filter: `uid=eq.${uid}`,
+      },
+      (payload) => {
+        callback(payload.new);
+      }
+    )
+    .subscribe();
+
+  return () => supabase.removeChannel(channel);
+}
 
