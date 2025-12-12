db65bc77e7927a4c774aaca6654e2217fa2fc8ae
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 15:31:58 +0800

    Delete docs/js/user-panel.js

--- docs/js/user-panel.js
+++
@@ -1,9 +0,0 @@
-export function initUserPanel(container) {
-  container.innerHTML = `
-    <div style="padding: 16px;">
-      <h3>User Panel</h3>
-      <p>User information goes here…</p>
-    </div>
-  `;
-}
-
