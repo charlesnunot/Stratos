b7ebe5b05082b02eda2a98c5dcc2d6fa40a05cdb
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 19:22:30 +0800

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -6,19 +6,16 @@ buttons.forEach(btn => {
     const panelId = btn.getAttribute('data-panel');
 
     if (panelId === 'panel1') { // Home 按钮
-      // 隐藏动态面板
       dynamicPanel.style.flex = '0 0 0';
       dynamicPanel.style.width = '0';
       dynamicPanel.style.padding = '0';
       dynamicPanel.style.overflow = 'hidden';
     } else {
-      // 显示动态面板
       dynamicPanel.style.flex = '0 0 300px';
       dynamicPanel.style.width = '300px';
       dynamicPanel.style.padding = '20px';
       dynamicPanel.style.overflow = 'auto';
 
-      // 显示对应面板内容
       const panels = dynamicPanel.querySelectorAll('.panel-section');
       panels.forEach(panel => panel.classList.remove('active'));
       const targetPanel = document.getElementById(panelId);
