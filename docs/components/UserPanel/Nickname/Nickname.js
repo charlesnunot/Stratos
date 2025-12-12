866a8723016c6b8bcdd60ac437139194c1cab744
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 21:58:01 +0800

    Update Nickname.js

--- docs/components/UserPanel/Nickname/Nickname.js
+++ docs/components/UserPanel/Nickname/Nickname.js
@@ -1,1 +1,12 @@
+export function initNickname(containerId, name = "Keyong") {
+  const container = document.getElementById(containerId);
+  if (!container) return;
+
+  fetch('./components/UserPanel/Nickname/Nickname.html')
+    .then(res => res.text())
+    .then(html => {
+      container.innerHTML = html;
+      container.querySelector(".nickname").textContent = name;
+    });
+}
 
