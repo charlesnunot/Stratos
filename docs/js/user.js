2b121b80df5b0c233e4382455f4d0f97308a2208
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 10:47:31 +0800

    Update user.js

--- docs/js/user.js
+++ docs/js/user.js
@@ -1,1 +1,16 @@
+// js/user.js
+async function loadSidebar() {
+  const container = document.getElementById("sidebar-container");
+  if (!container) return;
+
+  try {
+    const resp = await fetch("components/sidebar.html");
+    const html = await resp.text();
+    container.innerHTML = html;
+  } catch (err) {
+    console.error("加载 sidebar 失败:", err);
+  }
+}
+
+loadSidebar();
 
