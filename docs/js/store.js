b75a49114cef064be70c58aa9366e4be7545be1d
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 16:04:42 +0800

    Update store.js

--- docs/js/store.js
+++ docs/js/store.js
@@ -5,18 +5,18 @@ let state = {
 
 const listeners = [];
 
-export function getState(){ return state; }
+export function getState() { return state; }
 
-export function setState(newState){
-  state = {...state,...newState};
-  listeners.forEach(fn=>fn(state));
+export function setState(newState) {
+  state = { ...state, ...newState };
+  listeners.forEach(fn => fn(state));
 }
 
-export function subscribe(fn){
+export function subscribe(fn) {
   listeners.push(fn);
 }
 
-// detect mobile
-export function detectMobile(){
+// helper to detect mobile
+export function detectMobile() {
   return window.innerWidth <= 880;
 }
