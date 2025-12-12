eedc8daf48f66cfbdd747c9d6c78a810683e2f11
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 13:43:09 +0800

    Update sidebar.js

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -1,10 +1,10 @@
 export function initSidebar(container) {
   container.innerHTML = `
-    <button title="Home"><i class="fas fa-home"></i></button>
-    <button title="Search"><i class="fas fa-search"></i></button>
-    <button title="Messages"><i class="fas fa-comment-alt"></i></button>
-    <button title="Notifications"><i class="fas fa-bell"></i></button>
-    <button title="Settings"><i class="fas fa-cog"></i></button>
+    <button title="Home"><i class="fa-solid fa-house"></i></button>
+    <button title="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+    <button title="Messages"><i class="fa-solid fa-comment"></i></button>
+    <button title="Notifications"><i class="fa-solid fa-bell"></i></button>
+    <button title="Settings"><i class="fa-solid fa-gear"></i></button>
   `;
 
   container.querySelectorAll('button').forEach(btn => {
