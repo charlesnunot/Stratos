21ccbf9f6b067306fe982086e56e4f33dd4625b7
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 22:26:25 +0800

    Update UserPanel.js

--- docs/components/UserPanel/UserPanel.js
+++ docs/components/UserPanel/UserPanel.js
@@ -1,4 +1,4 @@
-import { initAvatarWrapper, setAppOnline, setAppOffline } from './AvatarWrapper/AvatarWrapper.js';
+import { initAvatarWrapper } from './AvatarWrapper/AvatarWrapper.js';
 import { initNickname } from './Nickname/Nickname.js';
 import { initUserStats } from './UserStats/UserStats.js';
 import { initUserBio } from './UserBio/UserBio.js';
@@ -19,10 +19,7 @@ export function initUserPanel() {
   initNickname("nickname-container", "Keyong");
   initUserStats("stats-container", { following: 102, followers: 230, likes: 58 });
   initUserBio("bio-container",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    "Lorem ipsum dolor sit amet...",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
   );
 }
-
-// ✅ 对外导出状态点方法
-export { setAppOnline, setAppOffline };
