61f2d7c0e87abb8b7c86610b6d73986a57b5e26e
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 13:36:48 +0800

    Update content.js

--- docs/js/content.js
+++ docs/js/content.js
@@ -1,1 +1,15 @@
+export function initContent(container) {
+  const posts = [
+    { title: 'User A', content: 'Hello, this is a post.' },
+    { title: 'User B', content: 'Another post content here.' },
+    { title: 'User C', content: 'More content...' },
+  ];
+
+  container.innerHTML = posts.map(p => `
+    <div class="post-card">
+      <h3>${p.title}</h3>
+      <p>${p.content}</p>
+    </div>
+  `).join('');
+}
 
