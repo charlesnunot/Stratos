9083d3b6604da53f3368a4afa39b8f34402243de
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 22:23:44 +0800

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,13 +1,18 @@
 import { initToolbar } from '../components/Toolbar/Toolbar.js';
-import { initUserPanel, setAppOnline, setAppOffline } from '../components/UserPanel/UserPanel.js';
+import { initUserPanel } from '../components/UserPanel/UserPanel.js';
 import { initDynamicPanel } from '../components/DynamicPanel/DynamicPanel.js';
 import { initMainContent } from '../components/MainContent/MainContent.js';
 
-// 初始化 Toolbar 并绑定面板切换
+// 先初始化 DynamicPanel，创建 panel 容器
+initDynamicPanel.init();
+
+// 然后初始化 UserPanel（panel-user 已存在 DOM）
+initUserPanel();
+
+// 初始化 MainContent
+initMainContent();
+
+// 初始化 Toolbar 并绑定点击事件
 initToolbar(panelId => {
   initDynamicPanel.showPanel(panelId);
 });
-
-// 初始化其他大组件
-initUserPanel();
-initMainContent();
