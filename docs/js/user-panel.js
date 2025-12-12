a78c014955cff562a5267d7f9172c3745da63fd2
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-12 15:17:15 +0800

    Update user-panel.js

--- docs/js/user-panel.js
+++ docs/js/user-panel.js
@@ -1,1 +1,9 @@
+export function initUserPanel(container) {
+  container.innerHTML = `
+    <div style="padding: 16px;">
+      <h3>User Panel</h3>
+      <p>User information goes here…</p>
+    </div>
+  `;
+}
 
