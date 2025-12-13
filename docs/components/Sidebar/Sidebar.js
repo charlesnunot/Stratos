f3975f9c13e66ebb02c7dee076221ab5d1fa590a
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 11:41:05 +0800

    Update Sidebar.js

--- docs/components/Sidebar/Sidebar.js
+++ docs/components/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 export async function mountSidebar(container) {
-  const html = await fetch('components/Sidebar/Sidebar.html').then(res => res.text());
+  // const html = await fetch('components/Sidebar/Sidebar.html').then(res => res.text());
+  const html = await fetch('./Sidebar.html').then(res => res.text());
   container.innerHTML = html;
 
   // CSS 相对于 Sidebar.js 自身
