7b24adcef8f510b504a8addacebbf2ddbbd3ed4d
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 11:37:00 +0800

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,4 @@
 // /docs/js/app.js
-import { mountSidebar } from '/Stratos/docs/components/Sidebar/Sidebar.js';
+import { mountSidebar } from 'components/Sidebar/Sidebar.js';
 
 mountSidebar(document.getElementById('sidebar-root'));
