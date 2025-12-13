c70568d49af318e0657f66126744d3c6e0511751
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:28:18 +0800

    Update PostsFeed.js

--- docs/components/Posts/PostsFeed.js
+++ docs/components/Posts/PostsFeed.js
@@ -1,1 +1,12 @@
+// docs/components/Posts/PostsFeed.js
+export function mountPostsFeed(container, posts) {
+  container.innerHTML = ''; // 清空内容
+
+  posts.forEach(post => {
+    const div = document.createElement('div');
+    div.className = 'post';
+    div.textContent = post;
+    container.appendChild(div);
+  });
+}
 
