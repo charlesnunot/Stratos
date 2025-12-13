c79790b8ccbf6d0f1a46ddf459ed4595fbaa6908
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 16:02:32 +0800

    Update MarketFollowing.js

--- docs/components/Posts/MarketFollowing.js
+++ docs/components/Posts/MarketFollowing.js
@@ -1,1 +1,13 @@
+// docs/components/Posts/MarketFollowing.js
+import { mountPostsFeed } from './PostsFeed.js';
+
+export function mountMarketFollowing(container) {
+  const posts = [
+    'Market Following Item 1',
+    'Market Following Item 2',
+    'Market Following Item 3'
+  ];
+
+  mountPostsFeed(container, posts);
+}
 
