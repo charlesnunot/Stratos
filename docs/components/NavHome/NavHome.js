09edca3fcc7df3e0680dd40b6e8e30d9e2ab9084
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 10:52:28 +0800

    Update NavHome.js

--- docs/components/NavHome/NavHome.js
+++ docs/components/NavHome/NavHome.js
@@ -1,11 +1,12 @@
+// docs/components/NavHome/NavHome.js
 export async function mountNavHome(container) {
   // HTML
-  const html = await fetch('./NavHome.html')  // 相对于 NavHome.js 自身目录
+  const html = await fetch('./NavHome.html')  // 相对于 NavHome.js
     .then(res => res.text());
   container.innerHTML = html;
 
   // CSS
-  loadCSS('./NavHome.css');  // 相对于 NavHome.js 自身目录
+  loadCSS('./NavHome.css');  // 相对于 NavHome.js
 
   // 点击事件
   const btn = container.querySelector('.nav-item');
