c25c03ec7d3c1786ae3bb3265d455bc735c3edc9
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 10:00:39 +0800

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,8 @@
+import { mountSidebar } from '../components/Sidebar/Sidebar.js';
+import { mountMain } from '../components/Main/Main.js';
+import { mountExtra } from '../components/Extra/Extra.js';
+
+mountSidebar(document.getElementById('sidebar-root'));
+mountMain(document.getElementById('main-root'));
+mountExtra(document.getElementById('extra-root'));
 
