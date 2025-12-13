cdb8d7b61cb9de743a6d6c3f1fd57cee2ec5f5db
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 15:29:01 +0800

    Update TabPage.js

--- docs/components/TabPage/TabPage.js
+++ docs/components/TabPage/TabPage.js
@@ -1,1 +1,6 @@
+<!-- docs/components/TabPage/TabPage.html -->
+<div class="home-container">
+  <div class="home-header" id="tab-header"></div>
+  <div class="home-content" id="tab-content"></div>
+</div>
 
