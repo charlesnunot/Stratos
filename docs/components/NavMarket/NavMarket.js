ddb9a18b1ccbb4f41cd442fc805117f2100ca3b0
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 15:55:05 +0800

    Update NavMarket.js

--- docs/components/NavMarket/NavMarket.js
+++ docs/components/NavMarket/NavMarket.js
@@ -1,9 +1,7 @@
 // docs/components/NavMarket/NavMarket.js
-
 export function mountNavMarket(container) {
   if (!container) return;
 
-  // 只绑定点击事件，不改 HTML
   container.addEventListener('click', () => {
     window.dispatchEvent(new CustomEvent('sidebar:navigate', { detail: { page: 'market' } }));
   });
