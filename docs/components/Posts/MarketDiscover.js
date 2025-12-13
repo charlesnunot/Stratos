42129a9c30ed80b901579ddb7785ea02b8a4bed8
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 15:14:34 +0800

    Update MarketDiscover.js

--- docs/components/Posts/MarketDiscover.js
+++ docs/components/Posts/MarketDiscover.js
@@ -1,1 +1,13 @@
+// docs/components/Posts/MarketDiscover.js
+import { mountPostsFeed } from './PostsFeed.js';
+
+export function mountMarketDiscover(container) {
+  const posts = [
+    'Market Discover Item 1',
+    'Market Discover Item 2',
+    'Market Discover Item 3'
+  ];
+
+  mountPostsFeed(container, posts);
+}
 
