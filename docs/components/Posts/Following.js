46ce912852972de08724a411430937614140456c
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:20:55 +0800

    Update Following.js

--- docs/components/Posts/Following.js
+++ docs/components/Posts/Following.js
@@ -1,1 +1,10 @@
+const baseURL = new URL('.', import.meta.url);
+
+export function mountFollowing(container) {
+  container.innerHTML = `
+    <div class="post">Following Post 1</div>
+    <div class="post">Following Post 2</div>
+    <div class="post">Following Post 3</div>
+  `;
+}
 
