f40002e21066e7713d2aa32173da771b8de95ffa
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 16:17:51 +0800

    Update Discover.js

--- docs/components/Posts/Discover.js
+++ docs/components/Posts/Discover.js
@@ -1,6 +1,11 @@
 import { mountPostsFeed } from './PostsFeed.js';
 
 export function mountDiscover(container) {
-  const posts = ['Discover Post 1', 'Discover Post 2', 'Discover Post 3'];
+  const posts = [
+    { title: 'Discover Post 1', author: 'Alice', time: '1h ago', excerpt: 'This is the first discover post.' },
+    { title: 'Discover Post 2', author: 'Bob', time: '2h ago', excerpt: 'This is the second discover post.' },
+    { title: 'Discover Post 3', author: 'Charlie', time: '3h ago', excerpt: 'This is the third discover post.' }
+  ];
+
   mountPostsFeed(container, posts);
 }
