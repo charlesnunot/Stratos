773bcddaf917ca6c39d9c1c396222b3cd6a15e33
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 15:54:36 +0800

    Update NavHome.js

--- docs/components/NavHome/NavHome.js
+++ docs/components/NavHome/NavHome.js
@@ -1,9 +1,7 @@
 // docs/components/NavHome/NavHome.js
-
 export function mountNavHome(container) {
   if (!container) return;
 
-  // 只绑定点击事件，不改 HTML
   container.addEventListener('click', () => {
     window.dispatchEvent(new CustomEvent('sidebar:navigate', { detail: { page: 'home' } }));
   });
