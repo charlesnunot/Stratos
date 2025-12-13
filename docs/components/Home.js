930cf4e8bad13bff7a69787b8e7e8d8ff0f2704e
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:04:36 +0800

    Update Home.js

--- docs/components/Home.js
+++ docs/components/Home.js
@@ -1,1 +1,24 @@
+// docs/components/Home/Home.js
+const baseURL = new URL('.', import.meta.url);
+
+export async function mountHome(container) {
+  if (!container) return;
+
+  // 加载 HTML
+  const html = await fetch(new URL('Home.html', baseURL)).then(res => res.text());
+  container.innerHTML = html;
+
+  // 可选加载 CSS
+  loadCSS(new URL('Home.css', baseURL));
+}
+
+// CSS 加载函数
+function loadCSS(href) {
+  const url = href.toString();
+  if (document.querySelector(`link[href="${url}"]`)) return;
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = url;
+  document.head.appendChild(link);
+}
 
