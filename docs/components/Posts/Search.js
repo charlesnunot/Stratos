2dad1f711f07f1c7632a6edfc805f0de766a9084
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:22:06 +0800

    Update Search.js

--- docs/components/Posts/Search.js
+++ docs/components/Posts/Search.js
@@ -1,1 +1,10 @@
+const baseURL = new URL('.', import.meta.url);
+
+export function mountSearch(container) {
+  container.innerHTML = `
+    <div class="post">Search Post 1</div>
+    <div class="post">Search Post 2</div>
+    <div class="post">Search Post 3</div>
+  `;
+}
 
