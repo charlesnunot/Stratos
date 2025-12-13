3cb80729112f78726c6fd48ea5a85e70fca2eaab
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:58:36 +0800

    Update Following.js

--- docs/components/Posts/Following.js
+++ docs/components/Posts/Following.js
@@ -1,11 +1,6 @@
-// Following.js
 import { mountPostsFeed } from './PostsFeed.js';
 
 export function mountFollowing(container) {
-  const posts = [
-    'Following Post 1',
-    'Following Post 2',
-    'Following Post 3'
-  ];
+  const posts = ['Following Post 1', 'Following Post 2', 'Following Post 3'];
   mountPostsFeed(container, posts);
 }
