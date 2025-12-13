c5f900505728181ef03038617c7ada684664f704
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-13 13:20:02 +0800

    Update Discover.js

--- docs/components/Posts/Discover.js
+++ docs/components/Posts/Discover.js
@@ -1,1 +1,10 @@
+const baseURL = new URL('.', import.meta.url);
+
+export function mountDiscover(container) {
+  container.innerHTML = `
+    <div class="post">Discover Post 1</div>
+    <div class="post">Discover Post 2</div>
+    <div class="post">Discover Post 3</div>
+  `;
+}
 
