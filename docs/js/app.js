fb568d6455512e6c6b6c9b974662afb20cbc9b40
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-14 10:03:53 +0800

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,5 @@
 // /docs/js/app.js
+import { supabase, getCurrentUser, onAuthChange } from '../store/supabase.js'
+import { setUser, clearUser, getUser, subscribe } from '../store/userManager.js'
 import { mountSidebar } from '../components/Sidebar/Sidebar.js';
 mountSidebar(document.getElementById('sidebar-root'));
