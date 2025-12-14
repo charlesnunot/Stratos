c9ec31cd8d10d6af8e30ff510fb4935487e22ec2
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-14 09:43:16 +0800

    Update actions.js

--- docs/store/actions.js
+++ docs/store/actions.js
@@ -1,1 +1,41 @@
+// docs/store/actions.js
+import { state } from './state.js';
+import { notify } from './subscribers.js';
+
+export const actions = {
+  setUser(user) {
+    state.user = { ...state.user, ...user };
+    notify(state, 'user');
+  },
+
+  logout() {
+    state.user = {
+      uid: null,
+      username: null,
+      avatar: null,
+      isOnline: false
+    };
+    notify(state, 'user');
+  },
+
+  setCurrentPage(page) {
+    state.ui.currentPage = page;
+    notify(state, 'ui.currentPage');
+  },
+
+  setMessages(messages) {
+    state.messages.list = messages;
+    notify(state, 'messages.list');
+  },
+
+  setCurrentMessage(message) {
+    state.messages.currentMessage = message;
+    notify(state, 'messages.currentMessage');
+  },
+
+  setMessageTab(tab) {
+    state.messages.currentTab = tab;
+    notify(state, 'messages.currentTab');
+  }
+};
 
