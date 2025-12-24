93e90c8517cd1d22851d03c92ccc31698371f51f
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-24 09:04:07 +0800

    Update systemMessageBootstrap.js

--- docs/store/systemMessageBootstrap.js
+++ docs/store/systemMessageBootstrap.js
@@ -1,1 +1,13 @@
+// docs/store/systemMessageBootstrap.js
+import { fetchSystemMessages } from './systemMessageApi.js'
+import { setSystemMessages } from './systemMessageStore.js'
+
+export async function initSystemMessages(user) {
+  if (!user) return
+
+  const { system, dynamic } = await fetchSystemMessages(user.id)
+
+  setSystemMessages('system', system)
+  setSystemMessages('dynamic', dynamic)
+}
 
