112695d018241bed2193db66e29654386b4d1162
charlesnunot <125963941+charlesnunot@users.noreply.github.com>
2025-12-25 13:08:56 +0800

    Update Sidebar.js

--- docs/components/Sidebar/Sidebar.js
+++ docs/components/Sidebar/Sidebar.js
@@ -264,13 +264,9 @@
 
 
 // docs/components/Sidebar/Sidebar.js
-// docs/components/Sidebar/Sidebar.js
 import { mountLogo } from '../Logo/Logo.js'
 import { subscribe as subscribeUser } from '../../store/userManager.js'
-import {
-  subscribeSystemMessages,
-  getUnreadCount
-} from '../../store/systemMessageStore.js'
+import { subscribeSystemMessages, getUnreadCount } from '../../store/systemMessageStore.js'
 import { getPageState, savePageState } from '../../store/pageStateStore.js'
 
 const baseURL = new URL('.', import.meta.url)
@@ -352,9 +348,7 @@ async function mountSidebarBottom() {
       if (!mainRoot) return
       mainRoot.innerHTML = ''
       try {
-        const { mountAppDownload } = await import(
-          new URL('../AppDownload/AppDownload.js', baseURL)
-        )
+        const { mountAppDownload } = await import(new URL('../AppDownload/AppDownload.js', baseURL))
         mountAppDownload(mainRoot)
       } catch (err) {
         console.error('加载 App 下载页面失败:', err)
